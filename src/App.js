153dcbc17e5f5fdd9490352299ad02681b341a11
Romir Patel <romirpatel308@gmail.com>
2024-03-02 18:02:47 -0500

    ui

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import './App.css';
+import React from 'react';
 
-function App() {
+export function App(props) {
   return (
-    <div style="width: 100%; height: 100%; position: relative; background: white">
-    <div style="width: 91px; height: 105px; left: 341px; top: 198px; position: absolute"></div>
-    <img style="width: 186px; height: 148px; left: 106px; top: 207px; position: absolute; box-shadow: 0px 4px 20px 10px rgba(52.20, 136.99, 215.26, 0.48); border-radius: 40px" src="https://via.placeholder.com/186x148" />
-    <img style="width: 186px; height: 148px; left: 768px; top: 207px; position: absolute; box-shadow: 0px 4px 20px 10px rgba(52.20, 136.99, 215.26, 0.48); border-radius: 40px" src="https://via.placeholder.com/186x148" />
-    <div style="width: 186px; height: 148px; left: 954px; top: 207px; position: absolute; box-shadow: 0px 4px 20px rgba(52.20, 136.99, 215.26, 0.48); border-radius: 40px"></div>
-    <img style="width: 186px; height: 148px; left: 548px; top: 207px; position: absolute; box-shadow: 0px 4px 20px 10px rgba(52.20, 136.99, 215.26, 0.48); border-radius: 40px" src="https://via.placeholder.com/186x148" />
-    <img style="width: 186px; height: 148px; left: 327px; top: 207px; position: absolute; box-shadow: 0px 4px 20px 10px rgba(52.20, 136.99, 215.26, 0.48); border-radius: 40px" src="https://via.placeholder.com/186x148" />
-    <div style="width: 621px; height: 75px; left: -40px; top: 161px; position: absolute; text-align: center; color: black; font-size: 30px; font-family: Poppins; font-weight: 600; line-height: 16px; word-wrap: break-word">Opportunities For You</div>
-    <div style="width: 1310px; height: 72px; left: 79px; top: 550px; position: absolute">
-        <div style="width: 1310px; height: 72px; left: 0px; top: 0px; position: absolute; background: #B0AFAF"></div>
-        <div style="left: 21px; top: 14px; position: absolute"><span style="color: #004AAD; font-size: 48px; font-family: League Spartan; font-weight: 400; word-wrap: break-word">UD</span><span style="color: black; font-size: 48px; font-family: League Spartan; font-weight: 400; word-wrap: break-word">ash</span></div>
-        <div style="width: 269px; height: 70px; padding-left: 10px; padding-right: 10px; padding-top: 5px; padding-bottom: 5px; left: 1084px; top: 0px; position: absolute; justify-content: center; align-items: center; gap: 15px; display: inline-flex">
-            <div style="padding: 10px; justify-content: center; align-items: center; gap: 5px; display: flex">
-                <img style="width: 40px; height: 40px; border-radius: 9999px" src="https://via.placeholder.com/40x40" />
-                <div style="text-align: center; color: #101540; font-size: 14px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Ann Lee</div>
-            </div>
+    <div style={{ width: '100%', height: '100%', position: 'relative', background: 'white' }}>
+      <div style={{ width: '91px', height: '105px', left: '341px', top: '198px', position: 'absolute' }}></div>
+      <img style={{ width: '186px', height: '148px', left: '106px', top: '207px', position: 'absolute', boxShadow: '0px 4px 20px 10px rgba(52, 20, 136, 0.48)', borderRadius: '40px' }} src="https://via.placeholder.com/186x148" />
+      <img style={{ width: '186px', height: '148px', left: '768px', top: '207px', position: 'absolute', boxShadow: '0px 4px 20px 10px rgba(52, 20, 136, 0.48)', borderRadius: '40px' }} src="https://via.placeholder.com/186x148" />
+      <div style={{ width: '186px', height: '148px', left: '954px', top: '207px', position: 'absolute', boxShadow: '0px 4px 20px rgba(52, 20, 136, 0.48)', borderRadius: '40px' }}></div>
+      <img style={{ width: '186px', height: '148px', left: '548px', top: '207px', position: 'absolute', boxShadow: '0px 4px 20px 10px rgba(52, 20, 136, 0.48)', borderRadius: '40px' }} src="https://via.placeholder.com/186x148" />
+      <img style={{ width: '186px', height: '148px', left: '327px', top: '207px', position: 'absolute', boxShadow: '0px 4px 20px 10px rgba(52, 20, 136, 0.48)', borderRadius: '40px' }} src="https://via.placeholder.com/186x148" />
+      <div style={{ width: '621px', height: '75px', left: '-40px', top: '161px', position: 'absolute', textAlign: 'center', color: 'black', fontSize: '30px', fontFamily: 'Poppins', fontWeight: '600', lineHeight: '16px', wordWrap: 'break-word' }}>Opportunities For You</div>
+      <div style={{ width: '1310px', height: '72px', left: '79px', top: '550px', position: 'absolute' }}>
+        <div style={{ width: '1310px', height: '72px', left: '0px', top: '0px', position: 'absolute', background: '#B0AFAF' }}></div>
+        <div style={{ left: '21px', top: '14px', position: 'absolute' }}><span style={{ color: '#004AAD', fontSize: '48px', fontFamily: 'League Spartan', fontWeight: '400', wordWrap: 'break-word' }}>UD</span><span style={{ color: 'black', fontSize: '48px', fontFamily: 'League Spartan', fontWeight: '400', wordWrap: 'break-word' }}>ash</span></div>
+        <div style={{ width: '269px', height: '70px', paddingLeft: '10px', paddingRight: '10px', paddingTop: '5px', paddingBottom: '5px', left: '1084px', top: '0px', position: 'absolute', justifyContent: 'center', alignItems: 'center', gap: '15px', display: 'inline-flex' }}>
+          <div style={{ padding: '10px', justifyContent: 'center', alignItems: 'center', gap: '5px', display: 'flex' }}>
+            <img style={{ width: '40px', height: '40px', borderRadius: '9999px' }} src="https://via.placeholder.com/40x40" />
+            <div style={{ textAlign: 'center', color: '#101540', fontSize: '14px', fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word' }}>Ann Lee</div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
   );
 }
 
-export default App;
+
+export default App
