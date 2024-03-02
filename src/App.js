fbba2450b2645e3ddb0e38e81353159dd53cada3
Romir Patel <romirpatel308@gmail.com>
2024-03-02 17:45:42 -0500

    first ui change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> myudash </h1>
-      </header>
+    <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
+    <div style={{width: 91, height: 105, left: 341, top: 198, position: 'absolute'}} />
+    <img style={{width: 186, height: 148, left: 106, top: 207, position: 'absolute', boxShadow: '0px 4px 20px 10px rgba(52.20, 136.99, 215.26, 0.48)', borderRadius: 40}} src="https://via.placeholder.com/186x148" />
+    <img style={{width: 186, height: 148, left: 768, top: 207, position: 'absolute', boxShadow: '0px 4px 20px 10px rgba(52.20, 136.99, 215.26, 0.48)', borderRadius: 40}} src="https://via.placeholder.com/186x148" />
+    <div style={{width: 186, height: 148, left: 954, top: 207, position: 'absolute', boxShadow: '0px 4px 20px rgba(52.20, 136.99, 215.26, 0.48)', borderRadius: 40}}></div>
+    <img style={{width: 186, height: 148, left: 548, top: 207, position: 'absolute', boxShadow: '0px 4px 20px 10px rgba(52.20, 136.99, 215.26, 0.48)', borderRadius: 40}} src="https://via.placeholder.com/186x148" />
+    <img style={{width: 186, height: 148, left: 327, top: 207, position: 'absolute', boxShadow: '0px 4px 20px 10px rgba(52.20, 136.99, 215.26, 0.48)', borderRadius: 40}} src="https://via.placeholder.com/186x148" />
+    <div style={{width: 621, height: 75, left: -40, top: 161, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 30, fontFamily: 'Poppins', fontWeight: '600', lineHeight: 16, wordWrap: 'break-word'}}>Opportunities For You</div> <div style={{width: 1310, height: 72, left: 79, top: 550, position: 'absolute'}}>
+        <div style={{width: 1310, height: 72, left: 0, top: 0, position: 'absolute', background: '#B0AFAF'}} />
+        <div style={{left: 21, top: 14, position: 'absolute'}}><span style="color: '#004AAD', fontSize: 48, fontFamily: 'League Spartan', fontWeight: '400', wordWrap: 'break-word'">UD</span><span style="color: 'black', fontSize: 48, fontFamily: 'League Spartan', fontWeight: '400', wordWrap: 'break-word'">ash</span></div>
+        <div style={{width: 269, height: 70, paddingLeft: 10, paddingRight: 10, paddingTop: 5, paddingBottom: 5, left: 1084, top: 0, position: 'absolute', justifyContent: 'center', alignItems: 'center', gap: 15, display: 'inline-flex'}}>
+            <div style={{padding: 10, justifyContent: 'center', alignItems: 'center', gap: 5, display: 'flex'}}>
+                <img style={{width: 40, height: 40, borderRadius: 9999}} src="https://via.placeholder.com/40x40" />
+                <div style={{textAlign: 'center', color: '#101540', fontSize: 14, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Ann Lee</div>
+            </div>
+        </div>
     </div>
+</div>
   );
 }
 
