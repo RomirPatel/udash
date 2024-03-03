7226a1be8ccf5c6a765a9be21e9becac2c360476
Romir Patel <romirpatel308@gmail.com>
2024-03-03 00:05:38 -0500

    React UI

---
+++ src/components/Component/Component.jsx
@@ -0,0 +1,30 @@
+/*
+We're constantly improving the code you see. 
+Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
+*/
+
+import React from "react";
+import "./style.css";
+
+export const Component = ({ className }) => {
+  return (
+    <div className={`component ${className}`}>
+      <p className="u-dash">
+        <span className="text-wrapper">UD</span>
+        <span className="span">ash</span>
+      </p>
+      
+      <div className="other-links">
+        <div className="ellipse" />
+        <div className="profile">
+          <div className="div">ktrl-g</div>
+        </div>
+      </div>
+      <div className="frame">
+        <div className="text-wrapper-2">Home</div>
+        <div className="text-wrapper-3">Rewards</div>
+        <div className="rectangle-2" />
+      </div>
+    </div>
+  );
+};
---
+++ src/screens/Home/Home.jsx
@@ -0,0 +1,210 @@
+import React from "react";
+import { Component } from "../../components/Component";
+import { Rectangle } from "../../components/Rectangle";
+import "./style.css";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      
+      <div className="div-2">
+        <img className="img" alt="Rectangle" src="https://c.animaapp.com/tQKE7sJp/img/rectangle-6@2x.png" />
+        <div className="overlap">
+          <div className="text-wrapper-4">Opportunities For You</div>
+          <Rectangle className="rectangle-7" />
+          <Rectangle className="rectangle-instance" />
+        </div>
+        <Component className="component-1" />
+        <Rectangle className="rectangle-7-instance" />
+        <img className="rectangle-3" alt="Rectangle" src="https://c.animaapp.com/tQKE7sJp/img/rectangle-14@2x.png" />
+        <img className="rectangle-4" alt="Rectangle" src="https://c.animaapp.com/tQKE7sJp/img/rectangle-14@2x.png" />
+        <div className="overlap-group">
+          <div className="text-wrapper-4">Ending Soon</div>
+          <Rectangle className="design-component-instance-node" />
+          <Rectangle className="rectangle-5" />
+        </div>
+        <div className="overlap-2">
+          <div className="text-wrapper-4">New</div>
+          <Rectangle className="rectangle-6" />
+          <Rectangle className="rectangle-8" />
+        </div>
+        <Rectangle className="rectangle-9" />
+        <Rectangle className="rectangle-10" />
+        <div className="text-wrapper-5">Leaderboard</div>
+        <div className="frame-2">
+          <div className="overlap-3">
+            <div className="frame-3">
+              <div className="overlap-wrapper">
+                <div className="overlap-4">
+                  <img className="ellipse-2" alt="Ellipse" src="https://c.animaapp.com/tQKE7sJp/img/ellipse-2@2x.png" />
+                  <div className="bxs-badge">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-6">9</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="text-wrapper-7">Paul C. Ramos</div>
+            </div>
+            <div className="frame-4">
+              <div className="text-wrapper-8">5075</div>
+            </div>
+          </div>
+          <div className="frame-5">
+            <div className="bi-trophy-fill">
+              <div className="text-wrapper-9">1</div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-6">
+          <div className="overlap-3">
+            <div className="frame-3">
+              <div className="overlap-wrapper">
+                <div className="overlap-4">
+                  <img className="ellipse-2" alt="Ellipse" src="https://c.animaapp.com/tQKE7sJp/img/ellipse-2@2x.png" />
+                  <div className="bxs-badge">
+                    <div className="overlap-group-2">
+                      <div className="text-wrapper-6">9</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="text-wrapper-7">Paul C. Ramos</div>
+            </div>
+            <div className="frame-4">
+              <div className="text-wrapper-8">4109</div>
+            </div>
+          </div>
+          <div className="bi-trophy-fill-2">
+            <div className="text-wrapper-9">2</div>
+          </div>
+        </div>
+        <div className="frame-7">
+          <div className="overlap-3">
+            <div className="frame-3">
+              <div className="overlap-wrapper">
+                <div className="overlap-4">
+                  <img className="ellipse-2" alt="Ellipse" src="https://c.animaapp.com/tQKE7sJp/img/ellipse-2@2x.png" />
+                  <div className="bxs-badge">
+                    <div className="overlap-group-3">
+                      <div className="text-wrapper-6">9</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="text-wrapper-7">Paul C. Ramos</div>
+            </div>
+            <div className="frame-4">
+              <div className="text-wrapper-8">4009</div>
+            </div>
+          </div>
+          <div className="bi-trophy-fill-wrapper">
+            <div className="bi-trophy-fill-3">
+              <div className="text-wrapper-9">3</div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-8">
+          <div className="frame-9">
+            <div className="overlap-wrapper">
+              <div className="overlap-5">
+                <img className="ellipse-3" alt="Ellipse" src="https://c.animaapp.com/tQKE7sJp/img/ellipse-6@2x.png" />
+                <div className="bxs-badge">
+                  <div className="overlap-group-4">
+                    <div className="text-wrapper-6">9</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="text-wrapper-7">Jack L. Gregory</div>
+          </div>
+          <div className="frame-5">
+            <div className="akar-icons-circle">
+              <div className="overlap-group-5">
+                <div className="text-wrapper-10">4</div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-10">
+            <div className="text-wrapper-11">3874</div>
+          </div>
+        </div>
+        <div className="frame-11">
+          <div className="frame-9">
+            <div className="overlap-wrapper">
+              <div className="overlap-5">
+                <img className="ellipse-3" alt="Ellipse" src="https://c.animaapp.com/tQKE7sJp/img/ellipse-6@2x.png" />
+                <div className="bxs-badge">
+                  <div className="overlap-group-6">
+                    <div className="text-wrapper-6">9</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="text-wrapper-7">Jack L. Gregory</div>
+          </div>
+          <div className="frame-5">
+            <div className="akar-icons-circle">
+              <div className="overlap-group-7">
+                <div className="text-wrapper-10">4</div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-10">
+            <div className="text-wrapper-11">3874</div>
+          </div>
+        </div>
+        <div className="frame-12">
+          <div className="frame-9">
+            <div className="overlap-wrapper">
+              <div className="overlap-5">
+                <img className="ellipse-3" alt="Ellipse" src="https://c.animaapp.com/tQKE7sJp/img/ellipse-6@2x.png" />
+                <div className="bxs-badge">
+                  <div className="overlap-group-6">
+                    <div className="text-wrapper-6">9</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="text-wrapper-7">Jack L. Gregory</div>
+          </div>
+          <div className="frame-5">
+            <div className="akar-icons-circle">
+              <div className="overlap-group-7">
+                <div className="text-wrapper-10">4</div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-10">
+            <div className="text-wrapper-11">3874</div>
+          </div>
+        </div>
+        <div className="frame-13">
+          <div className="frame-9">
+            <div className="overlap-wrapper">
+              <div className="overlap-5">
+                <img className="ellipse-3" alt="Ellipse" src="https://c.animaapp.com/tQKE7sJp/img/ellipse-6@2x.png" />
+                <div className="bxs-badge">
+                  <div className="overlap-group-4">
+                    <div className="text-wrapper-6">9</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="text-wrapper-7">Jack L. Gregory</div>
+          </div>
+          <div className="frame-5">
+            <div className="akar-icons-circle">
+              <div className="overlap-group-5">
+                <div className="text-wrapper-10">4</div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-10">
+            <div className="text-wrapper-11">3874</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
